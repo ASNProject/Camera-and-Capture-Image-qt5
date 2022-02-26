9693e060291399a0b0879eabf3a5bae4043ef544
Arief Setyo Nugroho <49858542+ASNProject@users.noreply.github.com>
2022-02-26 21:03:33 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main_gui.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(897, 478)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(20, 20, 411, 381))
+        self.groupBox.setObjectName("groupBox")
+        self.label = QtWidgets.QLabel(self.groupBox)
+        self.label.setGeometry(QtCore.QRect(0, 30, 411, 351))
+        self.label.setFrameShape(QtWidgets.QFrame.Box)
+        self.label.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.label.setLineWidth(2)
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.groupBox_2 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_2.setGeometry(QtCore.QRect(460, 20, 411, 391))
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.label_2 = QtWidgets.QLabel(self.groupBox_2)
+        self.label_2.setGeometry(QtCore.QRect(0, 30, 411, 351))
+        self.label_2.setFrameShape(QtWidgets.QFrame.Box)
+        self.label_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.label_2.setLineWidth(2)
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 410, 121, 30))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(460, 410, 411, 30))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Plat Number Detection using LVQ"))
+        self.groupBox.setTitle(_translate("MainWindow", "Camera"))
+        self.groupBox_2.setTitle(_translate("MainWindow", "Result"))
+        self.pushButton.setText(_translate("MainWindow", "Show Camera"))
+        self.pushButton_3.setText(_translate("MainWindow", "Show Detection"))
+
+
+#if __name__ == "__main__":
+#    import sys
+#    app = QtWidgets.QApplication(sys.argv)
+#    MainWindow = QtWidgets.QMainWindow()
+#    ui = Ui_MainWindow()
+#    ui.setupUi(MainWindow)
+#    MainWindow.show()
+#    sys.exit(app.exec_())
+
---
+++ play.py
@@ -0,0 +1,93 @@
+"""
+In this example, we demonstrate how to create simple camera viewer using Opencv3 and PyQt5
+Author: Berrouba.A
+Last edited: 21 Feb 2018
+"""
+
+# import system module
+import sys
+
+# import some PyQt5 modules
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QImage
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QTimer
+
+# import Opencv module
+import cv2
+
+data = 0;
+
+from main import *
+
+class MainWindow(QWidget):
+    # class constructor
+    def __init__(self):
+        # call QWidget constructor
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.logic=0
+        self.value=1
+
+        # create a timer
+        self.timer = QTimer()
+        # set timer timeout callback function
+        self.timer.timeout.connect(self.viewCam)
+        # set control_bt callback clicked  function
+        self.ui.pushButton.clicked.connect(self.controlTimer)
+        self.ui.pushButton_3.clicked.connect(self.detectionLVQ)
+
+
+    def detectionLVQ(self):
+        self.logic=2
+    # view camera
+    def viewCam(self):
+        # read image in BGR format
+        ret, image = self.cap.read()
+        # convert image to RGB format
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # get image infos
+        height, width, channel = image.shape
+        step = channel * width
+        # create QImage from image
+        qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
+        # show image in img_label
+        self.ui.label.setPixmap(QPixmap.fromImage(qImg))
+                
+        if(self.logic==2):
+            self.value=self.value+1
+            cv2.imwrite('/home/pi/LVQ/image/1.jpg', image)
+                    
+            self.logic=1
+        
+
+    # start/stop timer
+    def controlTimer(self):
+        # if timer is stopped
+        if not self.timer.isActive():
+            # create video capture
+            self.cap = cv2.VideoCapture(0)
+            # start timer
+            self.timer.start(20)
+            # update control_bt text
+            self.ui.pushButton.setText("Stop Camera")
+        # if timer is started
+        else:
+            # stop timer
+            self.timer.stop()
+            # release video capture
+            self.cap.release()
+            # update control_bt text
+            self.ui.pushButton.setText("Show Camera")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    # create and show mainWindow
+    mainWindow = MainWindow()
+    mainWindow.show()
+
+    sys.exit(app.exec_())
